36a6b4c3ac7eeedfefd3548474f41d4307aee593
Rachel Buchta <rachelbuchta@yahoo.com>
2020-11-05 16:04:49 -0700

    Add js file, HTML file, CSS file

---
+++ src/player.js
@@ -0,0 +1,15 @@
+class Player {
+  constructor(playerId, token, wins) {
+    this.playerId = playerId;
+    this.token = token;
+    this.wins = [];
+  }
+
+  saveWinsToStorage() {
+
+  }
+
+  retrieveWinsFromStorage() {
+    
+  }
+}
