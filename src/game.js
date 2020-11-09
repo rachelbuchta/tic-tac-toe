2296ab2786a4879f0725080fd2f09827dbc90fc6
Rachel Buchta <rachelbuchta@yahoo.com>
2020-11-09 16:32:08 -0700

    Add function to display winner and draw message and fix playGame function

--- src/game.js
+++ src/game.js
@@ -1,19 +1,19 @@
 class Game {
   constructor() {
-    this.playerOne = new Player("one", "X")
-    this.playerTwo = new Player("two", "O")
+    this.playerOne = new Player("one", "🌝")
+    this.playerTwo = new Player("two", "🪐")
     this.currentTurn = null;
-    this.board =  ["", "", "", "", "","","","",""]
+    this.board = ["", "", "", "", "", "", "", "", ""]
     this.winningCombos = [
-     [0,1,2],
-     [3,4,5],
-     [6,7,8],
-     [0,4,8],
-     [2,4,6],
-     [0,3,6],
-     [1,4,7],
-     [2,5,8]
-   ]
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8]
+    ]
     this.gameData = []
     this.gameRunning = false;
     this.playerWin = false;
@@ -21,37 +21,38 @@ class Game {
     this.turns = 0;
   }
 
-assignPlayer() {
- this.gameRunning = true;
- if(this.currentTurn === null) {
-   this.currentTurn = this.playerOne.token;
- }
+  assignPlayer() {
+    this.gameRunning = true;
+    if (this.currentTurn === null) {
+      this.currentTurn = this.playerOne.token;
+    }
 
- return this.currentTurn;
+    return this.currentTurn;
 
-}
+  }
 
   togglePlayers() {
-    if(this.currentTurn === this.playerOne.token) {
+    if (this.currentTurn === this.playerOne.token) {
       this.currentTurn = this.playerTwo.token
     } else if (this.currentTurn === this.playerTwo.token) {
-     this.currentTurn = this.playerOne.token
+      this.currentTurn = this.playerOne.token
     }
     return this.currentTurn
   }
 
-makeMove(index) {
-  var currentMove = this.currentTurn
-  var emptySquare = ""
-  if (this.board[index] === emptySquare) {
-    this.board[index] += currentMove
-    this.turns ++
-    return this.board
-  }
+  makeMove(index) {
+    var currentMove = this.currentTurn
+    var emptySquare = ""
+    if (this.board[index] === emptySquare) {
+      this.togglePlayers()
+      this.board[index] += currentMove
+      this.turns++
+    }
+    this.gameDrawCheck()
   }
 
   populatePlayerData(index) {
-    if(this.currentTurn === this.playerOne.token) {
+    if (this.currentTurn === this.playerOne.token) {
       this.playerOne.playerData.push(index)
       return this.playerOne.playerData
     } else if (this.currentTurn === this.playerTwo.token) {
@@ -60,48 +61,47 @@ makeMove(index) {
     }
   }
 
-gameComboCheck() {
-  if (this.playerOne.playerData.length === 3 || this.playerTwo.playerData.length === 3) {
-  for (var i = 0; i < this.winningCombos.length; i++) {
-    var combo = this.winningCombos[i];
-    for(var j = 0; j < combo.length; j ++) {
-      if(combo[j] === this.playerOne.playerData) {
-        this.playerWin = true;
-        this.playerOne.wins++
-      } else if(combo[j] === this.playerTwo.playerData) {
-        this.playerWin = true;
-        this.playerTwo.wins++
+  gameComboCheck() {
+    if (this.playerOne.playerData.length === 3 || this.playerTwo.playerData.length === 3) {
+      for (var i = 0; i < this.winningCombos.length; i++) {
+        var combo = this.winningCombos[i];
+        for (var j = 0; j < combo.length; j++) {
+          if (combo[j] === this.playerOne.playerData) {
+            this.playerWin = true;
+            this.playerOne.wins++
+          } else if (combo[j] === this.playerTwo.playerData) {
+            this.playerWin = true;
+            this.playerTwo.wins++
+          }
+        }
       }
-      }
+    } else {
+      this.togglePlayers()
     }
-  } else {
-    this.togglePlayers()
   }
-}
-//array prortype .includes
+  //array prortype .includes
 
 
   // with each move did they win? Comparing stored moves to winning combonation object each combo is an array of three (8 combos)
- // returns a boolean if false then toggle player
-// if true game ends and restarts after a couple seconds
-// change respective scores (score model) push into players array what there winning combos
+  // returns a boolean if false then toggle player
+  // if true game ends and restarts after a couple seconds
+  // change respective scores (score model) push into players array what there winning combos
 
-gameDrawCheck() {
-  if(this.turns === 9) {
-    this.playerDraw = true
-    this.gameRunning = false
-    return "It's A Draw"
-}
-}
-
-restartGame() {
-  if (this.playerWin === true || this.playerDraw === true) {
-    this.turns = 0
-    this.board = ["", "", "", "", "","","","",""]
-    this.playerOne.playerData = []
-    this.playerTwo.playerData = []
+  gameDrawCheck() {
+    if (this.turns === 9 && this.playerWin === false) {
+      this.playerDraw = true
+      this.gameRunning = false
+    }
   }
-}
+
+  restartGame() {
+    if (this.playerWin === true || this.playerDraw === true) {
+      this.turns = 0
+      this.board = ["", "", "", "", "", "", "", "", ""]
+      this.playerOne.playerData = []
+      this.playerTwo.playerData = []
+    }
+  }
 }
 
 // shoudld i push each players move to its own stored data array and then check them agains winning comboas?
--- src/main.js
+++ src/main.js
@@ -1,37 +1,51 @@
-
 var currentGame = new Game()
 //querySelectors
 var gameBoard = document.querySelector(".game-board")
 var clickedSquare = document.querySelectorAll(".box")
-
+var gameStatus = document.querySelector("h1")
+var playerOneWins = document.querySelector(".player-one-wins")
+var playerTwoWins = document.querySelector(".player-two-wins")
 
 
 
 //eventListeners
-window.addEventListener("load", playGame)
-gameBoard.addEventListener("click", selectSquare)
+window.addEventListener("load", beginGame)
+gameBoard.addEventListener("click", playGame)
 
-
-function playGame(index) {
-currentGame.assignPlayer()
+function beginGame() {
+  currentGame.assignPlayer()
+  gameStatus.innerText = `It's ${currentGame.currentTurn} Turn!`
 }
 
-function selectSquare(event) {
-console.log(event.target.id)
-var squareIndex = event.target.id[event.target.id.length -1]
-if(currentGame.board.includes(currentGame.currentTurn) === false) {
+function playGame(event) {
+  var squareIndex = event.target.id[event.target.id.length - 1]
   currentGame.makeMove(squareIndex)
-  console.log(squareIndex)
-  for (var i = 0; i < clickedSquare.length; i ++) {
+  for (var i = 0; i < clickedSquare.length; i++) {
     clickedSquare[i].innerHTML = currentGame.board[i]
-    }
-    currentGame.populatePlayerData(squareIndex)
-
-    currentGame.togglePlayers()
-}
+  }
+  currentGame.populatePlayerData(squareIndex)
+  chooseMessage()
 }
 
+function displayDrawMessage() {
+    gameStatus.innerHTML = "It's A Draw!"
+  }
 
+function togglePlayerMessage() {
+  gameStatus.innerText = `It's ${currentGame.currentTurn} Turn!`
+}
+
+function chooseMessage() {
+  if (currentGame.gameRunning === true) {
+    togglePlayerMessage()
+  } else if (currentGame.playerDraw === true) {
+    displayDrawMessage()
+  }
+}
+
+// if the current square includes a token, don't let it be clicked and dont toggle players
+// if the square doesnt include a token, let it be clicked, make move and toggle players
+  // if(currentGame.board[squareIndex].includes(currentGame.currentTurn) === false) {
 
 
 
